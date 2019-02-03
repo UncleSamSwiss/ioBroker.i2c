1d1832dd3e03cb72160a5985500753e5e5f087e4
Samuel Weibel <samuel.weibel@gmail.com>
2019-02-03 15:42:02 +0100

    Implemented emtpy stub for BME280.

---
+++ admin/devices/BME280.js
@@ -0,0 +1,33 @@
+﻿function BME280(name) {
+    DeviceBase.call(this, name);
+
+    this.addresses.push(0x76);
+    this.addresses.push(0x77);
+    
+    this.template = '<tr><td><label for="{{:#parent.parent.data.address}}-pollingInterval">{{t:"Polling Interval (sec)"}}</label></td><td class="admin-icon"></td>';
+    this.template += '<td><input type="text" id="{{:#parent.parent.data.address}}-pollingInterval" data-link="BME280.pollingInterval" /></td></tr>';
+}
+
+BME280.prototype = Object.create(DeviceBase.prototype);
+BME280.prototype.constructor = BME280;
+
+BME280.prototype.prepareViewModel = function (device) {
+    device.BME280 = device.BME280 || {};
+    device.BME280.pollingInterval = device.BME280.pollingInterval || 10;
+
+    return device;
+};
+
+BME280.prototype.prepareModel = function (device) {
+    device.name = this.name;
+    return device;
+};
+
+// "exports" of all device types supported by this class
+deviceTypes.BME280 = new BME280('BME280');
+
+// translations
+systemDictionary['Polling Interval (sec)'] = {
+    "en": "Polling Interval (sec)",
+    "de": "Abfrage-Intervall (sec)"
+};
---
+++ devices/BME280.js
@@ -0,0 +1,65 @@
+﻿"use strict";
+
+function create(deviceConfig, i2cAdapter) {
+    return new BME280(deviceConfig, i2cAdapter);
+}
+
+function BME280(deviceConfig, i2cAdapter) {
+    this.address = deviceConfig.address;
+    this.name = deviceConfig.name || 'BME280';
+    this.hexAddress = i2cAdapter.toHexString(this.address);
+
+    this.config = deviceConfig.BME280;
+
+    this.i2cAdapter = i2cAdapter;
+    this.adapter = this.i2cAdapter.adapter;
+}
+
+BME280.prototype.start = function () {
+    var that = this;
+    that.debug('Starting');
+    that.adapter.extendObject(that.hexAddress, {
+        type: 'device',
+        common: {
+            name: this.hexAddress + ' (' + this.name + ')',
+            role: 'sensor'
+        },
+        native: that.config
+    });
+    
+    if (that.config.pollingInterval && parseInt(that.config.pollingInterval) > 0) {
+        this.pollingTimer = setInterval(function () { that.readCurrentValues(); }, parseInt(that.config.pollingInterval) * 1000);
+    }
+};
+
+BME280.prototype.stop = function () {
+    this.debug('Stopping');
+    clearInterval(this.pollingTimer);
+};
+
+PCF8574.prototype.readCurrentValues = function () {
+    this.debug('Reading current values');
+    try {
+        // TODO: implement
+    } catch (e) {
+        this.error("Couldn't read current values: " + e);
+    }
+};
+
+BME280.prototype.debug = function (message) {
+    this.adapter.log.debug('BME280 ' + this.address + ': ' + message);
+};
+
+BME280.prototype.error = function (message) {
+    this.adapter.log.error('BME280 ' + this.address + ': ' + message);
+};
+
+BME280.prototype.setStateAck = function (name, value) {
+    return this.i2cAdapter.setStateAck(this.hexAddress + '.' + name, value);
+};
+
+BME280.prototype.getStateValue = function (name) {
+    return this.i2cAdapter.getStateValue(this.hexAddress + '.' + name);
+};
+
+module.exports.create = create;
