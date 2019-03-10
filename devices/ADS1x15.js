69f7d37065667f0b068c100bc3ade46b1cb0738f
Samuel Weibel <samuel.weibel@gmail.com>
2019-03-10 18:00:52 +0100

    Moved constants and global definitions inside the class.

--- devices/ADS1x15.js
+++ devices/ADS1x15.js
@@ -3,112 +3,113 @@
 // this class is based on https://github.com/alphacharlie/node-ads1x15/blob/master/index.js
 // probably MIT license (not explicitely mentioned, but it is based on the Adafruit Python code which is MIT)
 
-// chip
-IC_ADS1015 = 0x00;
-IC_ADS1115 = 0x01;
-
-// Pointer Register
-ADS1015_REG_POINTER_MASK = 0x03;
-ADS1015_REG_POINTER_CONVERT = 0x00;
-ADS1015_REG_POINTER_CONFIG = 0x01;
-ADS1015_REG_POINTER_LOWTHRESH = 0x02;
-ADS1015_REG_POINTER_HITHRESH = 0x03;
-
-// Config Register
-ADS1015_REG_CONFIG_OS_MASK = 0x8000;
-ADS1015_REG_CONFIG_OS_SINGLE = 0x8000; // Write: Set to start a single-conversion
-ADS1015_REG_CONFIG_OS_BUSY = 0x0000; // Read: Bit = 0 when conversion is in progress
-ADS1015_REG_CONFIG_OS_NOTBUSY = 0x8000; // Read: Bit = 1 when device is not performing a conversion
-ADS1015_REG_CONFIG_MUX_MASK = 0x7000;
-ADS1015_REG_CONFIG_MUX_DIFF_0_1 = 0x0000; // Differential P = AIN0, N = AIN1 (default)
-ADS1015_REG_CONFIG_MUX_DIFF_0_3 = 0x1000; // Differential P = AIN0, N = AIN3
-ADS1015_REG_CONFIG_MUX_DIFF_1_3 = 0x2000; // Differential P = AIN1, N = AIN3
-ADS1015_REG_CONFIG_MUX_DIFF_2_3 = 0x3000; // Differential P = AIN2, N = AIN3
-ADS1015_REG_CONFIG_MUX_SINGLE_0 = 0x4000; // Single-ended AIN0
-ADS1015_REG_CONFIG_MUX_SINGLE_1 = 0x5000; // Single-ended AIN1
-ADS1015_REG_CONFIG_MUX_SINGLE_2 = 0x6000; // Single-ended AIN2
-ADS1015_REG_CONFIG_MUX_SINGLE_3 = 0x7000; // Single-ended AIN3
-ADS1015_REG_CONFIG_PGA_MASK = 0x0E00;
-ADS1015_REG_CONFIG_PGA_6_144V = 0x0000; // +/-6.144V range
-ADS1015_REG_CONFIG_PGA_4_096V = 0x0200; // +/-4.096V range
-ADS1015_REG_CONFIG_PGA_2_048V = 0x0400; // +/-2.048V range (default)
-ADS1015_REG_CONFIG_PGA_1_024V = 0x0600; // +/-1.024V range
-ADS1015_REG_CONFIG_PGA_0_512V = 0x0800; // +/-0.512V range
-ADS1015_REG_CONFIG_PGA_0_256V = 0x0A00; // +/-0.256V range
-ADS1015_REG_CONFIG_MODE_MASK = 0x0100;
-ADS1015_REG_CONFIG_MODE_CONTIN = 0x0000; // Continuous conversion mode
-ADS1015_REG_CONFIG_MODE_SINGLE = 0x0100; // Power-down single-shot mode (default)
-ADS1015_REG_CONFIG_DR_MASK = 0x00E0;
-ADS1015_REG_CONFIG_DR_128SPS = 0x0000; // 128 samples per second
-ADS1015_REG_CONFIG_DR_250SPS = 0x0020; // 250 samples per second
-ADS1015_REG_CONFIG_DR_490SPS = 0x0040; // 490 samples per second
-ADS1015_REG_CONFIG_DR_920SPS = 0x0060; // 920 samples per second
-ADS1015_REG_CONFIG_DR_1600SPS = 0x0080; // 1600 samples per second (default)
-ADS1015_REG_CONFIG_DR_2400SPS = 0x00A0; // 2400 samples per second
-ADS1015_REG_CONFIG_DR_3300SPS = 0x00C0; // 3300 samples per second (also 0x00E0)
-ADS1115_REG_CONFIG_DR_8SPS = 0x0000; // 8 samples per second
-ADS1115_REG_CONFIG_DR_16SPS = 0x0020; // 16 samples per second
-ADS1115_REG_CONFIG_DR_32SPS = 0x0040; // 32 samples per second
-ADS1115_REG_CONFIG_DR_64SPS = 0x0060; // 64 samples per second
-ADS1115_REG_CONFIG_DR_128SPS = 0x0080; // 128 samples per second
-ADS1115_REG_CONFIG_DR_250SPS = 0x00A0; // 250 samples per second (default)
-ADS1115_REG_CONFIG_DR_475SPS = 0x00C0; // 475 samples per second
-ADS1115_REG_CONFIG_DR_860SPS = 0x00E0; // 860 samples per second
-ADS1015_REG_CONFIG_CMODE_MASK = 0x0010;
-ADS1015_REG_CONFIG_CMODE_TRAD = 0x0000; // Traditional comparator with hysteresis (default)
-ADS1015_REG_CONFIG_CMODE_WINDOW = 0x0010; // Window comparator
-ADS1015_REG_CONFIG_CPOL_MASK = 0x0008;
-ADS1015_REG_CONFIG_CPOL_ACTVLOW = 0x0000; // ALERT/RDY pin is low when active (default)
-ADS1015_REG_CONFIG_CPOL_ACTVHI = 0x0008; // ALERT/RDY pin is high when active
-ADS1015_REG_CONFIG_CLAT_MASK = 0x0004; // Determines if ALERT/RDY pin latches once asserted
-ADS1015_REG_CONFIG_CLAT_NONLAT = 0x0000; // Non-latching comparator (default)
-ADS1015_REG_CONFIG_CLAT_LATCH = 0x0004; // Latching comparator
-ADS1015_REG_CONFIG_CQUE_MASK = 0x0003;
-ADS1015_REG_CONFIG_CQUE_1CONV = 0x0000; // Assert ALERT/RDY after one conversions
-ADS1015_REG_CONFIG_CQUE_2CONV = 0x0001; // Assert ALERT/RDY after two conversions
-ADS1015_REG_CONFIG_CQUE_4CONV = 0x0002; // Assert ALERT/RDY after four conversions
-ADS1015_REG_CONFIG_CQUE_NONE = 0x0003; // Disable the comparator and put ALERT/RDY in high state (default)
-
-// This is a javascript port of python, so use objects instead of dictionaries here 
-// These simplify and clean the code (avoid the abuse of if/elif/else clauses)
-var spsADS1115 = {
-    8:   ADS1115_REG_CONFIG_DR_8SPS,
-    16:  ADS1115_REG_CONFIG_DR_16SPS,
-    32:  ADS1115_REG_CONFIG_DR_32SPS,
-    64:  ADS1115_REG_CONFIG_DR_64SPS,
-    128: ADS1115_REG_CONFIG_DR_128SPS,
-    250: ADS1115_REG_CONFIG_DR_250SPS,
-    475: ADS1115_REG_CONFIG_DR_475SPS,
-    860: ADS1115_REG_CONFIG_DR_860SPS
-};
-
-var spsADS1015 = {
-    128:  ADS1015_REG_CONFIG_DR_128SPS,
-    250:  ADS1015_REG_CONFIG_DR_250SPS,
-    490:  ADS1015_REG_CONFIG_DR_490SPS,
-    920:  ADS1015_REG_CONFIG_DR_920SPS,
-    1600: ADS1015_REG_CONFIG_DR_1600SPS,
-    2400: ADS1015_REG_CONFIG_DR_2400SPS,
-    3300: ADS1015_REG_CONFIG_DR_3300SPS
-};
-
-// Dictionary with the programable gains
-
-var pgaADS1x15 = {
-    6144: ADS1015_REG_CONFIG_PGA_6_144V,
-    4096: ADS1015_REG_CONFIG_PGA_4_096V,
-    2048: ADS1015_REG_CONFIG_PGA_2_048V,
-    1024: ADS1015_REG_CONFIG_PGA_1_024V,
-    512:  ADS1015_REG_CONFIG_PGA_0_512V,
-    256:  ADS1015_REG_CONFIG_PGA_0_256V
-};
-
-
 function create(deviceConfig, i2cAdapter) {
     return new ADS1x15(deviceConfig, i2cAdapter);
 }
 
 function ADS1x15(deviceConfig, i2cAdapter) {
+    // constants
+
+    // chip
+    this.IC_ADS1015 = 0x00;
+    this.IC_ADS1115 = 0x01;
+
+    // Pointer Register
+    this.ADS1015_REG_POINTER_MASK = 0x03;
+    this.ADS1015_REG_POINTER_CONVERT = 0x00;
+    this.ADS1015_REG_POINTER_CONFIG = 0x01;
+    this.ADS1015_REG_POINTER_LOWTHRESH = 0x02;
+    this.ADS1015_REG_POINTER_HITHRESH = 0x03;
+
+    // Config Register
+    this.ADS1015_REG_CONFIG_OS_MASK = 0x8000;
+    this.ADS1015_REG_CONFIG_OS_SINGLE = 0x8000; // Write: Set to start a single-conversion
+    this.ADS1015_REG_CONFIG_OS_BUSY = 0x0000; // Read: Bit = 0 when conversion is in progress
+    this.ADS1015_REG_CONFIG_OS_NOTBUSY = 0x8000; // Read: Bit = 1 when device is not performing a conversion
+    this.ADS1015_REG_CONFIG_MUX_MASK = 0x7000;
+    this.ADS1015_REG_CONFIG_MUX_DIFF_0_1 = 0x0000; // Differential P = AIN0, N = AIN1 (default)
+    this.ADS1015_REG_CONFIG_MUX_DIFF_0_3 = 0x1000; // Differential P = AIN0, N = AIN3
+    this.ADS1015_REG_CONFIG_MUX_DIFF_1_3 = 0x2000; // Differential P = AIN1, N = AIN3
+    this.ADS1015_REG_CONFIG_MUX_DIFF_2_3 = 0x3000; // Differential P = AIN2, N = AIN3
+    this.ADS1015_REG_CONFIG_MUX_SINGLE_0 = 0x4000; // Single-ended AIN0
+    this.ADS1015_REG_CONFIG_MUX_SINGLE_1 = 0x5000; // Single-ended AIN1
+    this.ADS1015_REG_CONFIG_MUX_SINGLE_2 = 0x6000; // Single-ended AIN2
+    this.ADS1015_REG_CONFIG_MUX_SINGLE_3 = 0x7000; // Single-ended AIN3
+    this.ADS1015_REG_CONFIG_PGA_MASK = 0x0E00;
+    this.ADS1015_REG_CONFIG_PGA_6_144V = 0x0000; // +/-6.144V range
+    this.ADS1015_REG_CONFIG_PGA_4_096V = 0x0200; // +/-4.096V range
+    this.ADS1015_REG_CONFIG_PGA_2_048V = 0x0400; // +/-2.048V range (default)
+    this.ADS1015_REG_CONFIG_PGA_1_024V = 0x0600; // +/-1.024V range
+    this.ADS1015_REG_CONFIG_PGA_0_512V = 0x0800; // +/-0.512V range
+    this.ADS1015_REG_CONFIG_PGA_0_256V = 0x0A00; // +/-0.256V range
+    this.ADS1015_REG_CONFIG_MODE_MASK = 0x0100;
+    this.ADS1015_REG_CONFIG_MODE_CONTIN = 0x0000; // Continuous conversion mode
+    this.ADS1015_REG_CONFIG_MODE_SINGLE = 0x0100; // Power-down single-shot mode (default)
+    this.ADS1015_REG_CONFIG_DR_MASK = 0x00E0;
+    this.ADS1015_REG_CONFIG_DR_128SPS = 0x0000; // 128 samples per second
+    this.ADS1015_REG_CONFIG_DR_250SPS = 0x0020; // 250 samples per second
+    this.ADS1015_REG_CONFIG_DR_490SPS = 0x0040; // 490 samples per second
+    this.ADS1015_REG_CONFIG_DR_920SPS = 0x0060; // 920 samples per second
+    this.ADS1015_REG_CONFIG_DR_1600SPS = 0x0080; // 1600 samples per second (default)
+    this.ADS1015_REG_CONFIG_DR_2400SPS = 0x00A0; // 2400 samples per second
+    this.ADS1015_REG_CONFIG_DR_3300SPS = 0x00C0; // 3300 samples per second (also 0x00E0)
+    this.ADS1115_REG_CONFIG_DR_8SPS = 0x0000; // 8 samples per second
+    this.ADS1115_REG_CONFIG_DR_16SPS = 0x0020; // 16 samples per second
+    this.ADS1115_REG_CONFIG_DR_32SPS = 0x0040; // 32 samples per second
+    this.ADS1115_REG_CONFIG_DR_64SPS = 0x0060; // 64 samples per second
+    this.ADS1115_REG_CONFIG_DR_128SPS = 0x0080; // 128 samples per second
+    this.ADS1115_REG_CONFIG_DR_250SPS = 0x00A0; // 250 samples per second (default)
+    this.ADS1115_REG_CONFIG_DR_475SPS = 0x00C0; // 475 samples per second
+    this.ADS1115_REG_CONFIG_DR_860SPS = 0x00E0; // 860 samples per second
+    this.ADS1015_REG_CONFIG_CMODE_MASK = 0x0010;
+    this.ADS1015_REG_CONFIG_CMODE_TRAD = 0x0000; // Traditional comparator with hysteresis (default)
+    this.ADS1015_REG_CONFIG_CMODE_WINDOW = 0x0010; // Window comparator
+    this.ADS1015_REG_CONFIG_CPOL_MASK = 0x0008;
+    this.ADS1015_REG_CONFIG_CPOL_ACTVLOW = 0x0000; // ALERT/RDY pin is low when active (default)
+    this.ADS1015_REG_CONFIG_CPOL_ACTVHI = 0x0008; // ALERT/RDY pin is high when active
+    this.ADS1015_REG_CONFIG_CLAT_MASK = 0x0004; // Determines if ALERT/RDY pin latches once asserted
+    this.ADS1015_REG_CONFIG_CLAT_NONLAT = 0x0000; // Non-latching comparator (default)
+    this.ADS1015_REG_CONFIG_CLAT_LATCH = 0x0004; // Latching comparator
+    this.ADS1015_REG_CONFIG_CQUE_MASK = 0x0003;
+    this.ADS1015_REG_CONFIG_CQUE_1CONV = 0x0000; // Assert ALERT/RDY after one conversions
+    this.ADS1015_REG_CONFIG_CQUE_2CONV = 0x0001; // Assert ALERT/RDY after two conversions
+    this.ADS1015_REG_CONFIG_CQUE_4CONV = 0x0002; // Assert ALERT/RDY after four conversions
+    this.ADS1015_REG_CONFIG_CQUE_NONE = 0x0003; // Disable the comparator and put ALERT/RDY in high state (default)
+
+    // This is a javascript port of python, so use objects instead of dictionaries here 
+    // These simplify and clean the code (avoid the abuse of if/elif/else clauses)
+    this.spsADS1115 = {
+        8:   this.ADS1115_REG_CONFIG_DR_8SPS,
+        16:  this.ADS1115_REG_CONFIG_DR_16SPS,
+        32:  this.ADS1115_REG_CONFIG_DR_32SPS,
+        64:  this.ADS1115_REG_CONFIG_DR_64SPS,
+        128: this.ADS1115_REG_CONFIG_DR_128SPS,
+        250: this.ADS1115_REG_CONFIG_DR_250SPS,
+        475: this.ADS1115_REG_CONFIG_DR_475SPS,
+        860: this.ADS1115_REG_CONFIG_DR_860SPS
+    };
+
+    this.spsADS1015 = {
+        128:  this.ADS1015_REG_CONFIG_DR_128SPS,
+        250:  this.ADS1015_REG_CONFIG_DR_250SPS,
+        490:  this.ADS1015_REG_CONFIG_DR_490SPS,
+        920:  this.ADS1015_REG_CONFIG_DR_920SPS,
+        1600: this.ADS1015_REG_CONFIG_DR_1600SPS,
+        2400: this.ADS1015_REG_CONFIG_DR_2400SPS,
+        3300: this.ADS1015_REG_CONFIG_DR_3300SPS
+    };
+
+    // Dictionary with the programable gains
+    this.pgaADS1x15 = {
+        6144: this.ADS1015_REG_CONFIG_PGA_6_144V,
+        4096: this.ADS1015_REG_CONFIG_PGA_4_096V,
+        2048: this.ADS1015_REG_CONFIG_PGA_2_048V,
+        1024: this.ADS1015_REG_CONFIG_PGA_1_024V,
+        512:  this.ADS1015_REG_CONFIG_PGA_0_512V,
+        256:  this.ADS1015_REG_CONFIG_PGA_0_256V
+    };
+
+    // variables
     this.address = deviceConfig.address;
     this.name = deviceConfig.name || 'ADS1x15';
     this.hexAddress = i2cAdapter.toHexString(this.address);
@@ -135,9 +136,9 @@ ADS1x15.prototype.start = function () {
     });
 
     if (that.config.kind == 1015) {
-        that.ic = IC_ADS1015;
+        that.ic = this.IC_ADS1015;
     } else {
-        that.ic = IC_ADS1115;
+        that.ic = this.IC_ADS1115;
     }
     
     var hasEnabled = false;
@@ -146,13 +147,13 @@ ADS1x15.prototype.start = function () {
         channelConfig.index = i;
         switch (channelConfig.channelType) {
             case 'single':
-                channelConfig.mux = ADS1015_REG_CONFIG_MUX_SINGLE_0 + (0x1000 * i);
+                channelConfig.mux = this.ADS1015_REG_CONFIG_MUX_SINGLE_0 + (0x1000 * i);
                 break;
             case 'diffTo1':
-                channelConfig.mux = ADS1015_REG_CONFIG_MUX_DIFF_0_1;
+                channelConfig.mux = this.ADS1015_REG_CONFIG_MUX_DIFF_0_1;
                 break;
             case 'diffTo3':
-                channelConfig.mux = ADS1015_REG_CONFIG_MUX_DIFF_0_3 + (0x1000 * i);
+                channelConfig.mux = this.ADS1015_REG_CONFIG_MUX_DIFF_0_3 + (0x1000 * i);
                 break;
             default:
                 channelConfig.mux = 0;
@@ -225,45 +226,45 @@ ADS1x15.prototype.readAdc = function (index, callback) {
 
     // Disable comparator, Non-latching, Alert/Rdy active low
     // traditional comparator, single-shot mode
-    var config = ADS1015_REG_CONFIG_CQUE_NONE | ADS1015_REG_CONFIG_CLAT_NONLAT |
-        ADS1015_REG_CONFIG_CPOL_ACTVLOW | ADS1015_REG_CONFIG_CMODE_TRAD |
-        ADS1015_REG_CONFIG_MODE_SINGLE;
+    var config = this.ADS1015_REG_CONFIG_CQUE_NONE | this.ADS1015_REG_CONFIG_CLAT_NONLAT |
+        this.ADS1015_REG_CONFIG_CPOL_ACTVLOW | this.ADS1015_REG_CONFIG_CMODE_TRAD |
+        this.ADS1015_REG_CONFIG_MODE_SINGLE;
     config |= channelConfig.mux;
 
-    if (that.ic == IC_ADS1015) {
-        if (spsADS1015[channelConfig.samples]) {
-            config |= spsADS1015[channelConfig.samples];
+    if (that.ic == this.IC_ADS1015) {
+        if (this.spsADS1015[channelConfig.samples]) {
+            config |= this.spsADS1015[channelConfig.samples];
         } else {
             that.debug('Using default 250 SPS');
-            config |= ADS1015_REG_CONFIG_DR_250SPS;
+            config |= this.ADS1015_REG_CONFIG_DR_250SPS;
         }
     } else {
-        if (spsADS1115[channelConfig.samples]) {
-            config |= spsADS1115[channelConfig.samples];
+        if (this.spsADS1115[channelConfig.samples]) {
+            config |= this.spsADS1115[channelConfig.samples];
         } else {
             that.debug('Using default 250 SPS');
-            config |= ADS1115_REG_CONFIG_DR_250SPS;
+            config |= this.ADS1115_REG_CONFIG_DR_250SPS;
         }
     }
 
     // Set PGA/voltage range
-    if (pgaADS1x15[channelConfig.gain]) {
-      config |= pgaADS1x15[channelConfig.gain];
+    if (this.pgaADS1x15[channelConfig.gain]) {
+      config |= this.pgaADS1x15[channelConfig.gain];
     } else {
         that.debug('Using default PGA 6.144 V');
-        config |= ADS1015_REG_CONFIG_PGA_6_144V;
+        config |= this.ADS1015_REG_CONFIG_PGA_6_144V;
     }
 
-    that.writeWord(ADS1015_REG_POINTER_CONFIG, config);
+    that.writeWord(this.ADS1015_REG_POINTER_CONFIG, config);
     
     // Wait for the ADC conversion to complete
     // The minimum delay depends on the sps: delay >= 1s/sps
     // We add 1ms to be sure
     var delay = (1000 / channelConfig.samples) + 1;
     setTimeout(function() {
-        var result = that.readWord(ADS1015_REG_POINTER_CONVERT);
+        var result = that.readWord(this.ADS1015_REG_POINTER_CONVERT);
         var value;
-        if (that.ic == IC_ADS1015) {
+        if (that.ic == this.IC_ADS1015) {
             // Shift right 4 bits for the 12-bit ADS1015 and convert to V
             value = (result >> 4) * channelConfig.gain / 2.048;
         } else {
