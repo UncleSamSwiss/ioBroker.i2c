e2b9ae010d1865b6cc478de6759c1208bd40ed36
Samuel Weibel <samuel.weibel@gmail.com>
2020-09-03 13:25:51 +0200

    Added rule for checking floating promises

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,17 +5,18 @@ module.exports = {
         sourceType: 'module', // Allows for the use of imports
         project: './tsconfig.json',
         ecmaFeatures: {
-          jsx: true
-        }
+            jsx: true,
+        },
     },
     extends: [
         'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
         'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
         'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-        "plugin:react/recommended" // Supports React JSX
+        'plugin:react/recommended', // Supports React JSX
     ],
     plugins: [],
     rules: {
+        '@typescript-eslint/no-floating-promises': 'error',
         '@typescript-eslint/no-parameter-properties': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/no-use-before-define': [
